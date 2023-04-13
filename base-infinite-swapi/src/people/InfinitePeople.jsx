8930e7706a3313c86164ae860bed51259fa85577
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-04-13 18:41:26 +0300

    feat - infinite scroll query

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -9,5 +11,23 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
+  /* 
+    fetchNextPage: function that will be used to tell infinite scroll what function to run
+    when we want more data
+    hasNextPage: boolean (determines if there's anymore data to collect)
+   */
+
+  /* 
+    useInfiniteQuery arguments:
+    queryKey: sw-people
+    queryFunction: fed an object parameter that has pageParam as one of the properties  
+   */
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
   return <InfiniteScroll />;
 }
